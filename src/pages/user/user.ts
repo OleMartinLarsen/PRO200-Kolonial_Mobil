73efe1b559b5c94c764ad422719ef104fdd87b91
Alexander <garale16@student.westerdals.no>
2018-05-22 12:22:08 +0200

    merge conflic 2

--- src/pages/user/user.ts
+++ src/pages/user/user.ts
@@ -1,13 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 import { AngularFireAuth } from 'angularfire2/auth';
-<<<<<<< HEAD
-
 import { HomePage } from '../home/home';
 import { TabsPage } from '../tabs/tabs';
-=======
 import { GlobalFunctionsProvider } from '../../providers/global-functions/global-functions';
->>>>>>> 0439d7f411f0e9ba1ba89642991580a17b1d41b2
 
 @IonicPage()
 @Component({
@@ -40,20 +36,7 @@ export class UserPage
   {
     this.functions.makeToast("Logger ut...");
     this.af.auth.signOut();
-<<<<<<< HEAD
     this.navCtrl.setRoot(TabsPage); //Sends user to HomePage (main/frontpage)
-  }
-
-  makeToast(toastMessage :string)
-  {
-    this.toastCtrl.create({
-      message: toastMessage,
-      duration: 3000,
-      position: 'bottom'
-    }).present();
-=======
-    this.navCtrl.pop();
->>>>>>> 0439d7f411f0e9ba1ba89642991580a17b1d41b2
   }
 
   ionViewDidLoad() 
