b4e9006dd82d9f8a63793d4a65b21849f71848ff
Alexander <garale16@student.westerdals.no>
2018-05-22 15:00:49 +0200

    eod

--- src/providers/api/api.ts
+++ src/providers/api/api.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Token } from '@angular/compiler';
 
 @Injectable()
 export class ApiProvider 
@@ -9,30 +8,23 @@ export class ApiProvider
   {
   }
   
-  getApi(pID :number)
+  getProduct(pID :number)
   {
-    let endPoint = 'https://kolonial.no/api/v1/products/' + pID + '/';
-    
-    this.http.get(endPoint)
-    .map(res => 
-      {
-        console.log('res: ', res);
-        return res;
-      });
+    var productLink = 'https://kolonial.no/api/v1/products/' + pID + '/';
 
-      //Reddit version
-      return new Promise((resolve, reject) => 
-      {
-        this.http.get(endPoint)
-          .subscribe(
-          (resp) => 
-          {
-            resolve(resp);
-          },
-          (error) => 
-          {
-            reject(error);
-          });
-      });
+    //Reddit version
+    return new Promise((resolve, reject) => 
+    {
+      this.http.get(productLink)
+        .subscribe(
+        (resp) => 
+        {
+          resolve(resp);
+        },
+        (error) => 
+        {
+          reject(error);
+        });
+    });
   }
 }
