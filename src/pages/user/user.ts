f2d20bd3f6a36529bb4452af4e185b29de245196
Alexander <garale16@student.westerdals.no>
2018-05-23 16:01:04 +0200

    Bruekrdata i på brukersiden

--- src/pages/user/user.ts
+++ src/pages/user/user.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
-import { AngularFireAuth } from 'angularfire2/auth';
-import { HomePage } from '../home/home';
-import { TabsPage } from '../tabs/tabs';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { GlobalFunctionsProvider } from '../../providers/global-functions/global-functions';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { User } from '../../models/user';
+import { TabsPage } from '../tabs/tabs';
+import { Observable } from 'rxjs/Observable';
 
 @IonicPage()
 @Component({
@@ -12,19 +13,40 @@ import { GlobalFunctionsProvider } from '../../providers/global-functions/global
 })
 export class UserPage 
 {
-  private email :string;
+  private collection :AngularFirestoreCollection<User>;
+  private currentUserEmail: string;
+  private users :Observable<any[]>;
 
   constructor(public navCtrl: NavController, 
     public navParams: NavParams,
-    private af :AngularFireAuth,
+    private af :AngularFirestore,
     private functions :GlobalFunctionsProvider) 
   {
-    //Globalize
-    var user = af.auth.currentUser;
-    if (user != null) 
+    this.currentUserEmail = af.app.auth().currentUser.email;
+    
+    //NB: this method fetches the userdata async from the DB with a query, so it might take some time.
+    //TODO find a btter way to load userdata
+    this.collection = af.collection<User>('users', (ref) => 
     {
-      this.email = user.email;
-    }
+      //Get correct user (limit 1 in case of duplicate emails in database 
+      //(which shouldn't really happens as we use FirebaseAuth which will handle this automatically))
+      return ref.where('userEmail', '==', this.currentUserEmail).limit(1); 
+    });
+
+    this.users = this.collection.snapshotChanges()
+      .map(actions =>
+      {
+        return actions.map(action =>
+        {
+          let data = action.payload.doc.data() as User;
+          let id = action.payload.doc.id;
+          
+          return {
+            id, 
+            ...data
+          }
+        });
+      });
   }
 
   pushSettings()
@@ -35,7 +57,7 @@ export class UserPage
   logoutUser()
   {
     this.functions.makeToast("Logger ut...");
-    this.af.auth.signOut();
+    this.af.app.auth().signOut();
     this.navCtrl.setRoot(TabsPage); //Sends user to HomePage (main/frontpage)
   }
 
