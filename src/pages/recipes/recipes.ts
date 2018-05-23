9ce0ebf991c6fbf463f7fa19ea08a0193808a667
Alexander <garale16@student.westerdals.no>
2018-05-23 14:23:19 +0200

    api test
    does not work

--- src/pages/recipes/recipes.ts
+++ src/pages/recipes/recipes.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ApiProvider } from '../../providers/api/api';
 
 @IonicPage()
 @Component({
@@ -8,8 +9,27 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class RecipesPage 
 {
-  constructor(public navCtrl: NavController, public navParams: NavParams) 
+  private resp: string ="You can do it";
+
+  constructor(public navCtrl: NavController, 
+    public navParams: NavParams,
+    private api: ApiProvider) 
   {
+  }
+
+  loadData()
+  {
+    this.api.getData()
+      .then((resp2: any) =>
+      {
+        console.log("recipes resp: " + resp2);
+        this.resp = resp2.detail;
+      })
+      .catch((error2) =>
+      {
+        console.log("recipes error: " + error2);
+        this.resp = "Try again.";
+      });
   }
 
   ionViewDidLoad() 
--- src/providers/api/api.ts
+++ src/providers/api/api.ts
@@ -1,13 +1,52 @@
 import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+import apikeys from '../../app/apikeys';
 
 @Injectable()
 export class ApiProvider 
 {
+  private agent: string = apikeys.UserAgent;
+  private token: string = apikeys.Token;
+
   constructor(public http: HttpClient) 
   {
   }
   
+  getData()
+  {
+    return new Promise((resolve, reject) =>
+    {
+      //Headers 1 (seems to work)
+      var headers = new Headers();
+      headers.append("Accept",  "application/json");
+      headers.append("Content-Type", "application/json");
+
+      //"Usage"
+      headers.append("User-Agent", this.agent);
+      headers.append("X-Client-Token", this.token);
+      let options = ({ headers: headers });
+
+      //"Usage->Authentication" (probably not)
+      // headers.append("Cookie", "sessionid=" + this.token);
+
+      this.http.post("https://www.kolonial.no/api/v1/products/9329", options)
+        .subscribe(
+          (resp) => 
+          { 
+            console.log('getData: succ');
+            resolve(resp);
+          },
+          (error) => 
+          { 
+            console.log('getData: failed');
+            reject(error);
+          });
+      console.log("GetData end");
+    });
+  }
+
   getProduct(pID :number)
   {
     var productLink = 'https://kolonial.no/api/v1/products/' + pID + '/';
