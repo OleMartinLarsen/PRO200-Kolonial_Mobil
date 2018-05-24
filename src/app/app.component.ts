23e9dd48520d7d7dd4233a8f00d39c862de6be26
Alexander <garale16@student.westerdals.no>
2018-05-24 11:59:48 +0200

    Fjernet småkrasj pga. manglende API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,33 +2,20 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { AngularFirestore } from 'angularfire2/firestore';
-import { HomePage } from '../pages/home/home';
 
 @Component({
   templateUrl: 'app.html'
 })
 
-export class MyApp 
-{
-  rootPage: any;
+export class MyApp {
+  rootPage: string = 'AuthPage';
 
   constructor(platform: Platform,
     statusBar: StatusBar,
-    splashScreen: SplashScreen,
-    private af :AngularFirestore) 
-    {
-      const authObserver = af.app.auth().onAuthStateChanged((user) =>
-      {
-        //Send the user to loginpage if they're not logged in
-        if (user)
-          this.rootPage = HomePage;
-        else
-          this.rootPage = 'AuthPage';
-      });
+    splashScreen: SplashScreen) {
 
-    platform.ready().then(() => 
-    {
+
+    platform.ready().then(() => {
       statusBar.styleDefault();
       splashScreen.hide();
     });
