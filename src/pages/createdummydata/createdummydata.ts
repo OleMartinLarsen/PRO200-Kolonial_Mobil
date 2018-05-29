f4f5994eb791914dd7444edcad50deebc74ff7aa
Alexander <garale16@student.westerdals.no>
2018-05-29 11:12:28 +0200

    legge til varer ok

--- src/pages/createdummydata/createdummydata.ts
+++ src/pages/createdummydata/createdummydata.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AngularFirestoreCollection, AngularFirestore } from 'angularfire2/firestore';
+import { Ware } from '../../models/ware';
+import { GlobalFunctionsProvider } from '../../providers/global-functions/global-functions';
 
 @IonicPage()
 @Component({
@@ -8,9 +11,37 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CreatedummydataPage 
 {
-  constructor(public navCtrl: NavController, 
-    public navParams: NavParams) 
+  public wareCollection :AngularFirestoreCollection<Ware>;
+  public ware =
   {
+    name: "",
+    price: 0,
+    type: "",
+    img: ""
+  };
+
+  constructor(public navCtrl: NavController, 
+    public navParams: NavParams,
+    private af: AngularFirestore,
+    private functions: GlobalFunctionsProvider) 
+  {
+    this.wareCollection = af.collection<Ware>('wares');
+  }
+
+  saveWare()
+  {
+    this.wareCollection.add(
+      {
+        wareName: this.ware.name,
+        warePrice: this.ware.price,
+        wareType: this.ware.type,
+        // wareImg: this.ware.img
+      } as Ware);
+
+    this.functions.makeToast("Lagret!");
+    this.ware.name = "";
+    this.ware.price = 0;
+    this.ware.type = "";
   }
 
   ionViewDidLoad() 
