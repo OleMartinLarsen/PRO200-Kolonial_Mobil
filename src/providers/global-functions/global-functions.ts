ca0de3b8e95f0c0466f1da38a3932db42200a28e
Alexander <garale16@student.westerdals.no>
2018-06-07 11:45:59 +0200

    GlobalFunctionsProvider rydding

--- src/providers/global-functions/global-functions.ts
+++ src/providers/global-functions/global-functions.ts
@@ -5,7 +5,6 @@ import { ToastController } from 'ionic-angular';
 @Injectable()
 export class GlobalFunctionsProvider 
 {
-  //TODO clean up array/returns, remove unused functions
   private recipeHistory: Array<any> = []; //List of recipes ordered
   private recipeFavorites: Array<any> = []; //List og recipes favorited
   private recipeIngredients: Array<any> = []; //List of ingredients when adding a recipe
@@ -27,77 +26,159 @@ export class GlobalFunctionsProvider
     this.populateOneWeakAheadArray();
   }
 
-  //Days
-
   //TODO tests?
 
-  getRecipesCart()
+  //Unused, can be used for testing 
+  // getOneWeakAheadArrayDay(i: number)
+  // {
+  //   console.log("oneWeakAheadArray: " + this.oneWeakAheadArray.toString());
+  //   console.log("nextday test: " + this.getNextDay(30, 6));
+  //   return this.oneWeakAheadArray[i];
+  // }
+
+  // --- Toasts ---
+
+  makeToast(toastMessage :string)
   {
-    return this.recipesCart;
+    this.toastCtrl.create({
+      message: toastMessage,
+      duration: 3000,
+      position: 'middle'
+    }).present();
   }
 
-  getIngredientsCart()
+  // --- Create recipes (dummy data) --- 
+
+  addIngredientToRecipeIngredients(ingredient: any)
   {
-    this.addIngredientsToCart();
-    return this.ingredientsCart;
+    this.recipeIngredients.push(ingredient);
+    console.log(ingredient.wareName + " added to recipe");
   }
 
-  addRecipeToCart()
+  getRecipeIngredients()
   {
-    var days = this.getDayPlans();
-    var i;
-    for(i = 0; i < days.length; i++)
-    {
-      this.recipesCart.push(days[i].recipe);
-    }
+    return this.recipeIngredients;
   }
 
-  addIngredientsToCart()
+  clearRecipeIngredients()
   {
-    var days = this.getDayPlans();
-    var i;
-    var j;
-    for(i = 0; i < days.length; i++)
-    {
-      for(j = 0; j < days[i].recipe.recipeIngredients.length; j++)
-      {
-        console.log("Add ingredients: " + days[i].recipe.recipeIngredients[j].wareName);
-        this.ingredientsCart.push(days[i].recipe.recipeIngredients[j]);
-      }
-    }
+    this.recipeIngredients = [];
+    return true;
   }
 
-  getCartPrice()
+  addInstructionsToRecipeInstructions(instructions: any)
   {
-    var price: number = 0;
-    var cart: Array<any> = [];
-    var days = this.getDayPlans();
-    var i;
-    var j;
-    for(i = 0; i < days.length; i++)
-    {
-      for(j = 0; j < days[i].recipe.recipeIngredients.length; j++)
-      {
-        cart.push(days[i].recipe.recipeIngredients[j]);
-        price = price + parseInt(days[i].recipe.recipeIngredients[j].warePrice);
-      }
-    }
-    cart = [];
-    return price;
+    this.recipeInstructions.push(instructions);
+    console.log(instructions + " added to recipe");
   }
 
-  getRecipeOfPlannedDayInDayPlans(displaydate: any)
+  getRecipeInstructions()
   {
-    var days = this.getDayPlans();
-    let day = days.find(d => d.date === displaydate);
+    return this.recipeInstructions;
+  }
 
-    if(day)
-    {
-      var index = days.map((d) => { return d.date; }).indexOf(displaydate);
-      // console.log("index for displaydate: " + index + " and " + displaydate);
-      return days[index];
-    }
-    return false;
+  clearRecipeInstructions()
+  {
+    this.recipeInstructions = [];
+    return true;
+  }
+
+  // --- Create recipes (users recipes) --- 
+
+  addMyRecipes(recipe: any)
+  {
+    this.myRecipes.push(recipe);
+    console.log(recipe.name + " added to myRecipes");
+  }
+
+  getMyRecipes()
+  {
+    return this.myRecipes;
+  }
+
+  // --- Planning for dinner --- 
+
+  addRecipeToDayPlans(recipe: any)
+  {
+    this.dayPlans.push(recipe);
+    console.log(recipe.recipe.recipeName + " added to dayPlans");
+  }
+
+  getDayPlans()
+  {
+    return this.dayPlans;
+  }
+
+  setIsPlanning(planning: boolean)
+  {
+    this.isPlanning = planning;
+  }
+
+  getIsPlanning()
+  {
+    return this.isPlanning;
+  }
+
+  setDayPlanningFor(day: string)
+  {
+    this.planningFor = day;
+  }
+
+  getDayPlanningFor()
+  {
+    return this.planningFor;
+  }
+
+  // --- Set up days and dates for planner --- 
+
+  populateDaysArrayNo()
+  {
+    this.daysArrayNo.push("Mandag");
+    this.daysArrayNo.push("Tirsdag");
+    this.daysArrayNo.push("Onsdag");
+    this.daysArrayNo.push("Torsdag");
+    this.daysArrayNo.push("Fredag");
+    this.daysArrayNo.push("Lørdag");
+    this.daysArrayNo.push("Søndag");
+  }
+
+  getDayInNorwegian(day: number)
+  {
+    return this.daysArrayNo[day - 1];
+  }
+
+  populateOneWeakAheadArray()
+  {
+    var day = new Date().getDate();
+    var month = new Date().getMonth() + 1;
+    //Get the next week
+    var next0 = this.getNextDay(day - 1, month); //Today
+    var next1 = this.getNextDay(day++, month);
+    var next2 = this.getNextDay(day++, month);
+    var next3 = this.getNextDay(day++, month);
+    var next4 = this.getNextDay(day++, month);
+    var next5 = this.getNextDay(day++, month);
+    var next6 = this.getNextDay(day, month);
+
+    this.oneWeakAheadArray.push(next0);
+    this.oneWeakAheadArray.push(next1);
+    this.oneWeakAheadArray.push(next2);
+    this.oneWeakAheadArray.push(next3);
+    this.oneWeakAheadArray.push(next4);
+    this.oneWeakAheadArray.push(next5);
+    this.oneWeakAheadArray.push(next6);
+  }
+
+  getNextDayFromOneWeakAheadArray()
+  {
+    var res = this.oneWeakAheadArray[this.nextDayFromOneWeakAheadArrayIndex];
+    this.nextDayFromOneWeakAheadArrayIndex++;
+    return res;
+  }
+
+  getNextDayFromOneWeakAheadArrayIndex()
+  {
+    return this.nextDayFromOneWeakAheadArrayIndex;
   }
 
   getNextDay(day: number, month: number)
@@ -152,64 +233,6 @@ export class GlobalFunctionsProvider
     return dayNo + " " + day + "." + month;
   }
 
-  populateOneWeakAheadArray()
-  {
-    var day = new Date().getDate();
-    var month = new Date().getMonth() + 1;
-    //Get the next week
-    var next0 = this.getNextDay(day - 1, month); //Today
-    var next1 = this.getNextDay(day++, month);
-    var next2 = this.getNextDay(day++, month);
-    var next3 = this.getNextDay(day++, month);
-    var next4 = this.getNextDay(day++, month);
-    var next5 = this.getNextDay(day++, month);
-    var next6 = this.getNextDay(day, month);
-
-    this.oneWeakAheadArray.push(next0);
-    this.oneWeakAheadArray.push(next1);
-    this.oneWeakAheadArray.push(next2);
-    this.oneWeakAheadArray.push(next3);
-    this.oneWeakAheadArray.push(next4);
-    this.oneWeakAheadArray.push(next5);
-    this.oneWeakAheadArray.push(next6);
-  }
-
-  //Unused, can be used for testing 
-  // getOneWeakAheadArrayDay(i: number)
-  // {
-  //   console.log("oneWeakAheadArray: " + this.oneWeakAheadArray.toString());
-  //   console.log("nextday test: " + this.getNextDay(30, 6));
-  //   return this.oneWeakAheadArray[i];
-  // }
-
-  getNextDayFromOneWeakAheadArray()
-  {
-    var res = this.oneWeakAheadArray[this.nextDayFromOneWeakAheadArrayIndex];
-    this.nextDayFromOneWeakAheadArrayIndex++;
-    return res;
-  }
-
-  getNextDayFromOneWeakAheadArrayIndex()
-  {
-    return this.nextDayFromOneWeakAheadArrayIndex;
-  }
-
-  populateDaysArrayNo()
-  {
-    this.daysArrayNo.push("Mandag");
-    this.daysArrayNo.push("Tirsdag");
-    this.daysArrayNo.push("Onsdag");
-    this.daysArrayNo.push("Torsdag");
-    this.daysArrayNo.push("Fredag");
-    this.daysArrayNo.push("Lørdag");
-    this.daysArrayNo.push("Søndag");
-  }
-
-  getDayInNorwegian(day: number)
-  {
-    return this.daysArrayNo[day - 1];
-  }
-
   getWeekNumber()
   {
     var month = new Date().getMonth();
@@ -220,19 +243,37 @@ export class GlobalFunctionsProvider
     //Not accurate on a month to month basis, does not take leap year into account
     return Math.round((month * 4.348214) + (day / 7));
   }
-
-  //Toast
-
-  makeToast(toastMessage :string)
+  
+  getCurrentDay()
   {
-    this.toastCtrl.create({
-      message: toastMessage,
-      duration: 3000,
-      position: 'middle'
-    }).present();
+    return this.getDayInNorwegian(new Date().getDay());
   }
 
-  //Getters and setters
+  getCurrentDate()
+  {
+    return new Date().getDate() + "." + (new Date().getMonth() + 1);
+  }
+
+  getCurrentDayDate()
+  {
+    return this.getCurrentDay() + " " + this.getCurrentDate();
+  }
+
+  getRecipeOfPlannedDayInDayPlans(displaydate: any)
+  {
+    var days = this.getDayPlans();
+    let day = days.find(d => d.date === displaydate);
+
+    if(day)
+    {
+      var index = days.map((d) => { return d.date; }).indexOf(displaydate);
+      // console.log("index for displaydate: " + index + " and " + displaydate);
+      return days[index];
+    }
+    return false;
+  }
+
+  // --- History/Favorites handeling --- 
 
   addRecipeToHistory(recipe: any)
   {
@@ -254,94 +295,60 @@ export class GlobalFunctionsProvider
     return this.recipeFavorites;
   }
 
-  addIngredientToRecipeIngredients(ingredient: any)
-  {
-    this.recipeIngredients.push(ingredient);
-    console.log(ingredient.wareName + " added to recipe");
-  }
-
-  getRecipeIngredients()
-  {
-    return this.recipeIngredients;
-  }
-
-  clearRecipeIngredients()
-  {
-    this.recipeIngredients = [];
-    return true;
-  }
-
-  addInstructionsToRecipeInstructions(instructions: any)
-  {
-    this.recipeInstructions.push(instructions);
-    console.log(instructions + " added to recipe");
-  }
-
-  getRecipeInstructions()
-  {
-    return this.recipeInstructions;
-  }
-
-  clearRecipeInstructions()
-  {
-    this.recipeInstructions = [];
-    return true;
-  }
-
-  addRecipeToDayPlans(recipe: any)
-  {
-    this.dayPlans.push(recipe);
-    console.log(recipe.recipe.recipeName + " added to dayPlans");
-  }
-
-  getDayPlans()
-  {
-    return this.dayPlans;
-  }
-
-  setIsPlanning(planning: boolean)
-  {
-    this.isPlanning = planning;
-  }
-
-  getIsPlanning()
-  {
-    return this.isPlanning;
-  }
-
-  setDayPlanningFor(day: string)
-  {
-    this.planningFor = day;
-  }
-
-  getDayPlanningFor()
-  {
-    return this.planningFor;
-  }
+  // --- Cart handeling --- 
   
-  getCurrentDay()
+  getRecipesCart()
   {
-    return this.getDayInNorwegian(new Date().getDay());
+    return this.recipesCart;
   }
 
-  getCurrentDate()
+  getIngredientsCart()
   {
-    return new Date().getDate() + "." + (new Date().getMonth() + 1);
+    this.addIngredientsToCart();
+    return this.ingredientsCart;
   }
 
-  getCurrentDayDate()
+  addRecipeToCart()
   {
-    return this.getCurrentDay() + " " + this.getCurrentDate();
+    var days = this.getDayPlans();
+    var i;
+    for(i = 0; i < days.length; i++)
+    {
+      this.recipesCart.push(days[i].recipe);
+    }
   }
 
-  addMyRecipes(recipe: any)
+  addIngredientsToCart()
   {
-    this.myRecipes.push(recipe);
-    console.log(recipe.name + " added to myRecipes");
+    var days = this.getDayPlans();
+    var i;
+    var j;
+    for(i = 0; i < days.length; i++)
+    {
+      for(j = 0; j < days[i].recipe.recipeIngredients.length; j++)
+      {
+        console.log("Add ingredients: " + days[i].recipe.recipeIngredients[j].wareName);
+        this.ingredientsCart.push(days[i].recipe.recipeIngredients[j]);
+      }
+    }
   }
 
-  getMyRecipes()
+  getCartPrice()
   {
-    return this.myRecipes;
+    var price: number = 0;
+    var cart: Array<any> = [];
+    var days = this.getDayPlans();
+    var i;
+    var j;
+    for(i = 0; i < days.length; i++)
+    {
+      for(j = 0; j < days[i].recipe.recipeIngredients.length; j++)
+      {
+        cart.push(days[i].recipe.recipeIngredients[j]);
+        price = price + parseInt(days[i].recipe.recipeIngredients[j].warePrice);
+      }
+    }
+    cart = [];
+    return price;
   }
 }
