37452d9a180669473d1185e517636e9d3077ac6a
RobinLind <robin_flaks@hotmail.com>
2018-06-12 16:43:15 +0200

    styling

--- src/pages/recipedetails/recipedetails.ts
+++ src/pages/recipedetails/recipedetails.ts
@@ -24,15 +24,6 @@ export class RecipedetailsPage
     recipe: ""
   }
 
-  private addDinnerButtonText = "";
-  private isFavorited: boolean = false;
-  private isPlanning: boolean = false;
-  private planningDay =
-  {
-    date: "",
-    recipe: ""
-  }
-
   constructor(public navCtrl: NavController, 
     public navParams: NavParams,
     private functions: GlobalFunctionsProvider) 
