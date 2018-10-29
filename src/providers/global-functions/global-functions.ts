355eada58f7f70e8751dc7c6b1fb3c89120e08d7
Alexander <garale16@student.westerdals.no>
2018-10-29 15:10:26 +0100

    weekplanning displayday fix + removed faultycss planningbutton
    PS: If you ahve issues with dataabse permissions, send me a mail, firebase updated api keys

--- src/providers/global-functions/global-functions.ts
+++ src/providers/global-functions/global-functions.ts
@@ -247,34 +247,34 @@ export class GlobalFunctionsProvider
 
     if(month == 2)
     {
-      if(day > 27)
+      if(day > 28)
       {
-        day = 1;
+        day -= 28;
         month++;
       }
     }
     else if(month == 4 || month == 6 || month == 9 || month == 11)
     {
-      if(day > 29)
+      if(day > 30)
       {
-        day = 1;
+        day -= 30;
         month++;
       }
     }
     else if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10)
     {
-      if(day > 30)
+      if(day > 31)
       {
-        day = 1;
+        day -= 31;
         month++;
       }
     }
     else if(month == 12)
     {
-      if(day > 30)
+      if(day > 31)
       {
-        day = 1;
-        month = 1;
+        day -= 31;
+        month -= 12;
       }
     }
 
